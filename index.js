2613216088b262c63d83906d2d920492f3ddff99
victor <victor.mbuthia@student.moringaschool.com>
2022-08-18 10:21:23 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,12 @@
+function divide() {
+}
+
+square = (parameter1) => parameter1 * parameter1; {
+    let x = 2   
+}
+
+add = (parameter1, parameter2) => parameter1 + parameter2; {
+    let a = 3
+    let b = 4
+}
+
